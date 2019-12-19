9811ba01ade78662b00aae2b6c88dd2bf5a87e8a
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-18 22:16:20 -0600

    Styling Navbar.js to be similar to mockup created by UI.

--- tieme-ndo/src/App.js
+++ tieme-ndo/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className='App'>
-			<NavBar />
+			<Navbar />
 			<img src={logo} className='App-logo' alt='logo' />
 			<p>
 				Edit <code>src/App.js</code> and save to reload.
--- tieme-ndo/src/components/NavBar.js
+++ tieme-ndo/src/components/NavBar.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import Button from './Button';
+import styled from 'styled-components';
 
-export default function NavBar() {
+const NavBar = styled.nav`
+	position: fixed;
+	width: 100%;
+	top: 0;
+	left: 0;
+	background: #333333;
+	z-index: 1;
+	font-size: 1.4rem;
+`;
+
+const FlexContainer = styled.div`
+	max-width: 120%;
+	display: flex;
+	margin: auto;
+	padding: 0 2%;
+	justify-content: space-evenly;
+	height: 5%;
+
+	a {
+		text-decoration: none;
+		color: #46d2a3;
+		width: 80%;
+		max-width: 100%;
+		border-bottom: 1px solid transparent;
+		margin: 0 1.5%;
+		text-decoration: none;
+		cursor: pointer;
+
+	&:hover {
+		background: #4e4e4e;
+		border-bottom: 1px solid #ffffff;
+	}
+`;
+
+// const NavLinks = styled.a``;
+
+export default function Navbar(props) {
 	return (
-		<nav>
+		<NavBar>
+			<FlexContainer>
+				<a href='https://www.github.com'>About Us</a>
+				<a href='https://www.github.com'>Stories</a>
+				<a href='https://www.github.com'>Staff</a>
+				<a href='https://www.github.com'>Contact</a>
+			</FlexContainer>
 			<Button>Log In</Button>
-			<Button primary>Sign Up</Button>
-		</nav>
+			<Button primary>Register</Button>
+		</NavBar>
 	);
 }
